d0acf2a50dbd9e03bc96c2dde568cd0102226c85
ReniPutriani <putrianir39@gmail.com>
2024-04-05 07:13:36 +0700

    learn: button + toaster

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
 
 export default function Home() {
+  const { toast } = useToast();
   return (
     <main>
       <aside></aside>
       <section>
-        
+        <Button onClick={() => {
+          toast({
+            title: "Hello",
+            description: "This is a toast message",
+            action: <Button>Undo</Button>,
+          });
+        }}>Click me</Button>
       </section>
     </main>
   );
